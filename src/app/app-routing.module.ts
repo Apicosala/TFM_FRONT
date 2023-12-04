1f59e164425582ec7114ca3666fe7b8cb0692878
Adrián Vicaria <adrianvicaria@gmail.com>
2023-12-04 21:45:07 +0100

    isAdmin guard funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () =>
-      import('./modules/admin/admin.module').then((m) => m.AdminModule), canActivateChild: [], 
+      import('./modules/admin/admin.module').then((m) => m.AdminModule), canActivateChild: [isAdminGuard], 
   },
   {
     path: 'clases',
