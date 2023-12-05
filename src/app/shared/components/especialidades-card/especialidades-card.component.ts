2b69e77201a406837b23da7a5359fea8a4070319
MarAltaba <maraltabarosas@gmail.com>
2023-12-05 12:50:23 +0100

    GoToProfesoresList

--- src/app/shared/components/especialidades-card/especialidades-card.component.ts
+++ src/app/shared/components/especialidades-card/especialidades-card.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class EspecialidadesCardComponent {
   @Input() oneEspecialidad: any
   applyOverlay: boolean = false;
+
+  getProfesores(){
+     console.log("Go to profesores")
+  }
 }
