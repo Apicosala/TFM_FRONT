f566b33c5b47e3aa604714285d5efc6099c3aabb
jmneko <jmunhiztarrio@gmail.com>
2023-12-08 17:23:20 +0100

    Solucionado probelma contraseña vacia

--- src/app/shared/components/form-usuarios/form-usuarios.component.ts
+++ src/app/shared/components/form-usuarios/form-usuarios.component.ts
@@ -49,11 +49,10 @@ export class FormUsuariosComponent {
 
         experiencia: new FormControl('', []),
 
-        pass: new FormControl('', [
+       pass: new FormControl('', [
           Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=[^\d_].*?\d)\w(\w|[!@#$%]){7,}/
           ),
         ]),
-
         newPass: new FormControl('', [
           Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=[^\d_].*?\d)\w(\w|[!@#$%]){7,}/
           ),]),
@@ -83,8 +82,6 @@ export class FormUsuariosComponent {
     }
   }
   
-  
-  
 
   ngOnInit(): void {
     
@@ -94,7 +91,7 @@ export class FormUsuariosComponent {
       let decodedToken = jwtDecode<PayLoad>(token);
       let id = decodedToken.user_id;
       this.perfilServices.getById(id).subscribe((data) => {
-
+        
         this.usuario = data[0];
 
         this.formUsuario = new FormGroup({
@@ -138,13 +135,23 @@ export class FormUsuariosComponent {
 
           },
           [this.controlPass]
-        );
+          );
       });
     }
   }
 
   getDataForm() {
-    if (this.usuario && this.usuario.id) { 
+    if (this.usuario && this.usuario.id) {
+      
+      // Verificamos si la nueva contraseña se ha ingresado
+    const nuevaContraseña = this.formUsuario.get('newPass')?.value;
+    const contraseñaActual = this.formUsuario.get('pass')?.value;
+
+    // Si la nueva contraseña se ha ingresado, la actualizamos
+    if (nuevaContraseña !== undefined && nuevaContraseña !== '' && nuevaContraseña !==  contraseñaActual) {
+      this.formUsuario.get('pass')?.setValue(nuevaContraseña, { emitEvent: false });
+    }
+
       this.perfilServices.update(this.formUsuario.value)
       .then((response: any) => {
         Swal.fire({
